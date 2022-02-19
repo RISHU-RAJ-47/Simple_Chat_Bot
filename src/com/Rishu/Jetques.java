a47cfbe40ca90ac79c85e41f6e8df40bb419611b
RISHU RAJ <rishugcraj1234@gmail.com>
2022-02-19 17:53:38 +0530

    Initial commit Complete SimpleChat Bot project.

---
+++ src/com/Rishu/Jetques.java
@@ -0,0 +1,53 @@
+package com.Rishu;
+import java.util.Scanner;
+public class Jetques{
+    public static void main (String[] args){
+        /* Scanner scanner = new Scanner(System.in);
+        int hourOfFirstMoment = scanner.nextInt();
+        int minOfFirstMoment = scanner.nextInt();
+        int secOfFirstMoment = scanner.nextInt();
+        int hourOfSecondMoment = scanner.nextInt();
+        int minOfSecondMoment = scanner.nextInt();
+        int secOfSecondMoment = scanner.nextInt();
+        int hr1InSec = hourOfFirstMoment * 60 * 60;
+        int min1InSec = minOfFirstMoment * 60;
+        int totalSecIn1stMom = hr1InSec + min1InSec + secOfFirstMoment;
+        int hr2InSec = hourOfSecondMoment * 60 * 60;
+        int min2InSec = minOfSecondMoment * 60;
+        int totalSecIn2ndMom = hr2InSec + min2InSec + secOfSecondMoment;
+        int totalSecPassed = totalSecIn2ndMom - totalSecIn1stMom;
+        System.out.println(totalSecPassed);*/
+
+        // second question
+        /*
+        * Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int firstDigitOfNum = number / 100;
+        int sep2ndDigit = number % 100;
+        int secondDigitOfNum = sep2ndDigit / 10;
+        int thirdDigitOfNum = sep2ndDigit % 10;
+        System.out.println(firstDigitOfNum + secondDigitOfNum + thirdDigitOfNum);*/
+        // ANOTHER QUESTION
+        /*Scanner scanner = new Scanner(System.in);
+        int H = scanner.nextInt();
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int day = (H - B) / (A - B) + ((H - B) % (A - B) + A - 1) / A;
+        System.out.println(day);*/
+        // TOTAL COST OF VACATION
+        /*Scanner scanner = new Scanner(System.in);
+        int durationInDays = scanner.nextInt();
+        int totalFoodCostPerDay = scanner.nextInt();
+        int oneWayFlightCost = scanner.nextInt();
+        int costOfOneNightInHotel = scanner.nextInt();
+        int flightCost = oneWayFlightCost * 2;
+        int numberOfNights = durationInDays - 1;
+        int totalCost = (totalFoodCostPerDay * durationInDays) + (flightCost) + (costOfOneNightInHotel * numberOfNights);
+        System.out.println(totalCost);
+         */
+        int n = 0;
+        System.out.print(n++);
+
+
+    }
+}
